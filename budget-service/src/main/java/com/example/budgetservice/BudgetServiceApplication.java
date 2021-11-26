510bdfb0493685b4efe20b98f785a2e11cc5c606
Katarzyna Wasik <kat.wasik@hotmail.com>
2021-11-26 18:18:52 +0100

    first commit

---
+++ budget-service/src/main/java/com/example/budgetservice/BudgetServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.budgetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class BudgetServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetServiceApplication.class, args);
+	}
+
+}
